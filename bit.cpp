4d2da4709c9e74823299f6090686afbe60cdd067
erev <edgar_arevalo@live.com.mx>
2017-02-24 18:32:59 -0600

    Add files via upload

---
+++ bit.cpp
@@ -0,0 +1,16 @@
+#include <iostream>       // std::cout
+#include <string>         // std::string
+#include <cstddef>        // std::size_t
+#include <bitset>         // std::bitset
+
+int main ()
+{
+  std::bitset<5> foo (std::string("01011"));
+
+  std::cout << "foo contains:\n";
+
+  for (std::size_t i=0; i<foo.size(); ++i)
+    std::cout << foo[i] << '\n';
+
+  return 0;
+}
